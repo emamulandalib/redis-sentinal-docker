cc447a428cfe9b0d5db718c552d7284298c2989a
Emamul Andalib <emamul.andalib@gmail.com>
2020-06-29 17:32:23 +0600

    - Master/Slave will run in bridge mode

--- entrypoints/master-entrypoint.sh
+++ entrypoints/master-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 sed -i "s/\$PASSWORD/$PASSWORD/g" /usr/local/etc/redis/redis.conf
-sed -i "s/\$PORT/$PORT/g" /usr/local/etc/redis/redis.conf
 
 redis-server /usr/local/etc/redis/redis.conf
